d1b30dffc5cee5e6a26379fc0981bc2b85314e69
MOULOUNGUI-Samuel <moulounguibienvenuesamara@gmail.com>
2025-05-12 16:38:28 +0200

    ajout des options

--- app/Http/Controllers/DashboardRHController.php
+++ app/Http/Controllers/DashboardRHController.php
@@ -17,21 +17,37 @@ class DashboardRHController extends Controller
     {
         //
         $entreprise_id = auth()->user()->entreprise_id;
-        $employes = User::where('entreprise_id', $entreprise_id)->get();
-        $pointages_oui = Pointage::whereHas('user', fn($query) => $query->where('entreprise_id', $entreprise_id))
-            ->where('date_arriver', now()->format('Y-m-d'))
-            ->get();
 
-        $users_non_existants = User::where('entreprise_id', $entreprise_id)
-            ->whereDoesntHave('pointage', function ($query) {
-                $query->whereDate('date_arriver', now()->format('Y-m-d'));
-            })
-            ->get();
+        $role_user = User::where('id', auth()->user()->id)->with('role')->first();
 
-        $pointage_intermediaires = PointagesIntermediaire::whereHas('pointage', fn($query) => $query->whereHas('user', fn($subQuery) => $subQuery->where('entreprise_id', auth()->user()->entreprise_id)))
-            ->whereHas('pointage', fn($query) => $query->where('date_arriver', now()->format('Y-m-d')))
-            ->get();
+        if ($role_user->role->nom !== 'Admin') {
+            $employes = User::where('entreprise_id', $entreprise_id)->get();
 
+            $pointages_oui = Pointage::whereHas('user', fn($query) => $query->where('entreprise_id', $entreprise_id))
+                ->where('date_arriver', now()->format('Y-m-d'))
+                ->get();
+
+            $users_non_existants = User::where('entreprise_id', $entreprise_id)
+                ->whereDoesntHave('pointage', function ($query) {
+                    $query->whereDate('date_arriver', now()->format('Y-m-d'));
+                })
+                ->get();
+
+            $pointage_intermediaires = PointagesIntermediaire::whereHas('pointage', fn($query) => $query->whereHas('user', fn($subQuery) => $subQuery->where('entreprise_id', auth()->user()->entreprise_id)))
+                ->whereHas('pointage', fn($query) => $query->where('date_arriver', now()->format('Y-m-d')))
+                ->get();
+        } else {
+            $employes = User::All();
+            $pointages_oui = Pointage::where('date_arriver', now()->format('Y-m-d'))->get();
+
+            $users_non_existants = User::whereDoesntHave('pointage', function ($query) {
+                    $query->whereDate('date_arriver', now()->format('Y-m-d'));
+                })
+                ->get();
+
+            $pointage_intermediaires = PointagesIntermediaire::whereHas('pointage', fn($query) => $query->where('date_arriver', now()->format('Y-m-d')))
+                ->get();
+        }
         return view("components.yodirh.dashboard", compact('employes', 'pointages_oui', 'users_non_existants', 'pointage_intermediaires'));
     }
 
