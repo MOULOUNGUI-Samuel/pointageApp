9d5775505d406d62737c5ece35c147def29ed6c0
MOULOUNGUI-Samuel <moulounguibienvenuesamara@gmail.com>
2025-10-13 13:30:34 +0200

    mobile debut front

--- resources/views/components/mobileApp/actualite.blade.php
+++ resources/views/components/mobileApp/actualite.blade.php
@@ -15,10 +15,6 @@
 <body class="">
     <main class="container py-8 min-h-dvh text-b300 bg-y50 dark:bg-darkY50 dark:text-white">
         <div class="px-6 flex justify-between items-center gap-4">
-            <a href="{{ route('connexion') }}"
-                class="size-12 bg-b300 text-white flex justify-center items-center rounded-full text-2xl dark:bg-darkB300">
-                <i class="ph ph-list" style="font-size:24px;"></i>
-            </a>
             <div
                 class="flex justify-between items-center flex-1 border border-n40 rounded-full p-3 gap-2 dark:border-darkN40 dark:bg-darkN20 bg-white dark:bg-darkN40">
                 <div class="">
@@ -41,14 +37,15 @@
                     </div>
                     <p class="mt-2">Chargement des actualités en cours...</p>
                 </div>
-                <div class="taggbox" style="width:100%;height:100%;overflow:auto;" data-widget-id="300959" data-website="1">
+                <div class="taggbox" style="width:100%;height:100%;overflow:auto;" data-widget-id="300959"
+                    data-website="1">
                 </div>
                 <script src="https://widget.taggbox.com/embed.min.js" type="text/javascript"></script>
                 <script>
                     document.addEventListener('DOMContentLoaded', function() {
                         const loadingElement = document.getElementById('taggbox-loading');
                         const taggboxElement = document.querySelector('.taggbox');
-        
+
                         // Afficher le contenu Taggbox après un délai pour s'assurer qu'il est chargé
                         setTimeout(() => {
                             if (loadingElement) loadingElement.style.display = 'none';
@@ -56,6 +53,34 @@
                         }, 2000); // Délai de 2 secondes avant de masquer le chargement
                     });
                 </script>
+            </div>
+        </div>
+        <div class="fixed bottom-0 left-0 right-0 container z-30">
+            <div class="p-6 bg-b300 rounded-t-3xl flex justify-between items-center">
+                <a href="#" class="flex flex-col gap-1 justify-center items-center">
+                    <i class="ph-fill ph-house text-3xl !leading-none text-y200"></i>
+                    <p class="text-xs font-semibold text-white">Actualité</p>
+                </a>
+                <a href="#" class="flex flex-col gap-1 justify-center items-center">
+                    <i class="ph ph-book-open text-3xl !leading-none text-white"></i>
+                    <p class="text-xs font-semibold text-white">A propos</p>
+                </a>
+                <a href="#" class="flex flex-col gap-1 justify-center items-center">
+                    <i class="ph ph-headset text-3xl !leading-none text-white"></i>
+                    <p class="text-xs font-semibold text-white">Contact</p>
+                </a>
+                <a href="{{route('connexion')}}" class="flex flex-col gap-1 justify-center items-center">
+                    <i class="
+                    ph ph-sign-in
+                     text-3xl 
+                    !leading-none text-white
+                    "></i>
+                    <p class="text-xs font-semibold text-white">Connexion</p>
+                </a>
+                <a href="settings.html" class="flex flex-col gap-1 justify-center items-center">
+                    <i class="ph ph-list text-3xl !leading-none text-white"></i>
+                    <p class="text-xs font-semibold text-white">Menu</p>
+                </a>
             </div>
         </div>
     </main>
